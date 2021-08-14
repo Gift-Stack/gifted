e7ab9d8a9e1aceab4e7f96bb07fc21fd591b3152
Gift-Stack <gkenny896@gmail.com>
2021-08-14 08:13:58 +0100

    Navbar done

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,44 @@
+import React, { useState } from 'react'
 import './styles.css'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  const [toggle, setToggle] = useState(false)
+  const [x, setX] = useState('')
+
+  const handleToggle = (prev: boolean, e: React.MouseEvent) => {
+    setToggle(!prev)
+    if (toggle === true) {
+      setX('')
+    } else if (prev === false) {
+      setX('x')
+    }
+  }
+  return (
+    <nav>
+      <div className="logo">Gift Opia</div>
+      {toggle && (
+        <ul>
+          <li>
+            <a href="somewhere.com">Home</a>
+          </li>
+          <li>
+            <a href="somewhere.com">Works</a>
+          </li>
+          <li>
+            <a href="somewhere.com">Contacts</a>
+          </li>
+        </ul>
+      )}
+      <div
+        className={`menu-icon d-lg-none js-menu-open menu-open-${x}`}
+        onClick={(e) => handleToggle(toggle, e)}
+      >
+        <span className="line"></span>
+        <span className="line"></span>
+        <span className="line"></span>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
